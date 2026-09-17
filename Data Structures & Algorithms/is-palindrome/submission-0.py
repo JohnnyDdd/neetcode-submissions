12c8495fbe5a916eb3858d0509dec179f9c59d41
Johnny-xd <158844724+JohnnyDdd@users.noreply.github.com>
2026-09-17 16:10:12 -0400

    Add: is-palindrome - submission-0

---
+++ Data Structures & Algorithms/is-palindrome/submission-0.py
@@ -0,0 +1,12 @@
+class Solution:
+    def isPalindrome(self, s: str) -> bool:
+        s = re.split('[^a-zA-Z0-9]',s)
+        s = ''.join(s).lower()
+        print(s)
+        i = 0
+        j = len(s) - 1
+        while i < j:
+            if s[i] != s[j]: return False
+            i+=1
+            j-=1
+        return True
